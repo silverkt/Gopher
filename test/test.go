387306f0efeadb198bbbd0315da2f67b933be84d
tsing.sun <tsing@tsingsundeMacBook-Pro.local>
2018-03-06 13:40:24 +0800

    float to string & circle

--- test/test.go
+++ test/test.go
@@ -1,6 +1,21 @@
+/**
+testa is a package for my test
+*/
 package testa
-import "fmt"
+import (
+	"fmt";
+	"strconv"
+)
 
+const Pi float64 = 3.1415926
+var testa int = 12
+var teststring string = "this is a good world"
 func Tt() {
-	fmt.Println("test pages import")
+	fmt.Println(strconv.Itoa(testa) + teststring + strconv.FormatFloat(Pi, 'E', -1, 64))
+	for i := 0; i < 20; i++ {
+		fmt.Println("this is round" + strconv.Itoa(i))
+		if i == 12 {
+			fmt.Println("twelve is a good girl")
+		}
+	}
 }
