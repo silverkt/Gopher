6e15e0f9da10410ddc948182a30a33acfbf1f247
silver.sun <itisagift@yeah.net>
2018-10-28 13:30:10 +0800

    write prototype of a web spider

---
+++ spider/spider.go
@@ -0,0 +1,94 @@
+package main;
+
+import (
+	"fmt";
+	"net/http";
+	"io/ioutil";
+	"regexp";
+	"time";
+	"strconv"
+)
+
+
+// h.p03.space/viewthread.php?tid=253453&page=21
+/// http://h.p03.space/attachments/180827171132291b5106ef17dd.jpg
+func main() {
+	for i := 1; i < 22; i++ {
+		res, _ := getHtml("http://h.p03.space/viewthread.php?tid=253453&page="+strconv.Itoa(i));
+		fmt.Println("http://h.p03.space/viewthread.php?tid=253453&page="+strconv.Itoa(i));
+		savePagedImg(res);
+	} 
+}
+
+
+/*
+获取当前页面图片列表并保存
+**/
+func savePagedImg(res string) {
+	//获取列表
+	imgList := getList(res);
+	for index, value := range imgList {
+		imgList[index] = "http://h.p03.space/"+value;
+		saveRes(imgList[index]); //保存图片
+		time.Sleep(time.Second);
+		fmt.Println(imgList[index]);
+	}
+	fmt.Println(" ");
+}
+
+/*
+获取91图片列表
+**/
+func getList(content string) []string {
+	re := regexp.MustCompile(`attachments/[a-zA-Z0-9]*(.jpg|.png)`);
+	return re.FindAllString(content, -1)
+}
+
+
+/*
+获取网络资源
+@params（url string：获取的源网址）
+@return（[]byte：资源内容； error：读取错误）
+**/
+func getResource(url string) ([]byte, error) {
+	res, err := http.Get(url);
+	if err != nil {
+		fmt.Println("Get URl Error");
+		ioutil.WriteFile("error_log.txt", []byte(url), 0x755);
+	}
+	defer res.Body.Close();
+	data, err := ioutil.ReadAll(res.Body);
+	return data, err
+}
+
+
+/*
+获取网页内容
+@params（url string：获取的源网址）
+@return（string：网页内容； error：读取错误）
+**/
+func getHtml(url string) (string, error) {
+	res, err := getResource(url);
+	return string(res), err
+}
+
+/*
+获取并保存网络资源
+**/
+func saveRes(url string) {
+	data, _ := getResource(url);
+	ioutil.WriteFile(getName(url), data, 0x755);
+}
+
+/*
+获取文件名
+@params （url string：获取的包含文件名的链接字符串）
+@return （string 文件名）
+**/
+func getName(url string) string {
+	re := regexp.MustCompile(`[a-zA-Z0-9]*(.jpg|.png)`);
+	return re.FindString(url);	
+}
+
+
+
