6fea68d1aa8687040b9e6cf7a857b547c3d32ef5
silver.sun <itisagift@yeah.net>
2018-11-12 19:50:40 +0800

    update

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -9,8 +9,8 @@ import (
 
 func HelloServer(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("Inside HelloServer handler");
-	// fmt.Fprintf(res, "Hello,"+req.URL.Path[1:]);
-	fmt.Fprintf(res, "<h1>sxq</h1><div>Hello World!!</div>");
+	fmt.Fprintf(res, "Hello,"+req.URL.Path[1:]);
+	//fmt.Fprintf(res, "<h1>sxq</h1><div>Hello World!!</div>");
 }
 
 func main() {
