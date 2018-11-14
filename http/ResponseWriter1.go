d5dd025ba0049aab1961179191e456380c837bd7
silver.sun <itisagift@yeah.net>
2018-11-14 13:01:38 +0800

    update

---
+++ http/ResponseWriter1.go
@@ -0,0 +1,61 @@
+package main;
+
+import (
+	//"fmt";
+	"net/http";
+	"io";
+	"log";
+)
+
+/**
+公共response设置
+*/
+type commonHandler struct {
+
+}
+func (this *commonHandler) myResponseWriter(w http.ResponseWriter) http.ResponseWriter  {
+	w.Header().Set("Content-Type", "text/html");
+	return w;
+}
+
+
+/**
+首页处理
+*/
+type indexHandler struct {
+	message string;
+	commonHandler;
+}
+func (this *indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w = this.myResponseWriter(w);
+	io.WriteString(w, this.message);
+}
+
+/**
+test处理
+*/
+type testHandler struct {
+	message string;
+}
+func (this *testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//w = myResponseWriter(w);
+	w.Header().Set("Content-Type", "text/plain");
+	io.WriteString(w, this.message);
+}
+
+
+
+
+
+
+
+func main() {
+	mux := http.NewServeMux();
+	mux.Handle("/", &indexHandler{message:"test<div style=background:#eee;>kskksksk</div>"});
+	mux.Handle("/test", &indexHandler{message:"test<div style=background:red;>testpage</div>"});
+	err := http.ListenAndServe(":12345", mux)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
