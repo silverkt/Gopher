23521193c0b7d80c1282590127631db880d8f5d6
silver.sun <itisagift@yeah.net>
2018-11-17 13:22:01 +0800

    template must web

--- html/template/templateMust/template.go
+++ html/template/templateMust/template.go
@@ -1,36 +1,61 @@
 package main
 
 import (
-	//"net/http"
+	"net/http"
 	"html/template"
-	"io/ioutil"
-	"os"
+    "io/ioutil"
+	// "os"
 	"time"
 
 	"fmt"
 )
 
-func main() {
+func test1Handler(w http.ResponseWriter, r *http.Request) {
 	t := template.New("第一个模板").Delims("[[", "]]") //创建一个模板,设置模板边界
 	t, _ = t.Parse("hello,[[.UserName]]\n")       //解析模板文件
 	data := map[string]interface{}{"UserName": template.HTML("<script>alert('you have been pwned')</script>")}
-	t.Execute(os.Stdout, data) //执行模板的merger操作，并输出到控制台
+	//w.Header().Set("Content-Type","text/html");
+	t.Execute(w, data) //执行模板的merger操作，并输出到控制台
 	fmt.Println(t.Name(), "\n\n")
+}
 
+func test2Handler(w http.ResponseWriter, r *http.Request) {
 	t2 := template.New("第二个模板")                         //创建模板
 	t2.Funcs(map[string]interface{}{"tihuan": tihuan}) //向模板中注入函数
 	bytes, _ := ioutil.ReadFile("tpl/test2.html")          //读文件
 	template.Must(t2.Parse(string(bytes)))             //将字符串读作模板 并Must错误处理
-	t2.Execute(os.Stdout, map[string]interface{}{"UserName": "你好世界"})
+	t2.Execute(w, map[string]interface{}{"UserName": "你好世界"})
 	fmt.Println("\n", t2.Name(), "\n")
 
-	t3, _ := template.ParseFiles("tpl/test1.html") //将一个文件读作模板
-	t3.Execute(os.Stdout, data)
-	fmt.Println("\n", t3.Name(), "\n") //模板名称
+}
 
+func test3Handler(w http.ResponseWriter, r *http.Request) {
+	t3, _ := template.ParseFiles("tpl/test1.html") //将一个文件读作模板
+	data := map[string]interface{}{"UserName": template.HTML("<script>alert('you have been pwned')</script>")}
+	t3.Execute(w, data)
+	fmt.Println("\n", t3.Name(), "\n") //模板名称
+}
+
+func test4Handler(w http.ResponseWriter, r *http.Request) {
 	t4, _ := template.ParseGlob("tpl/test1.html") //将一个文件读作模板
-	t4.Execute(os.Stdout, data)
+	data := map[string]interface{}{"UserName": template.HTML("<script>alert('you have been pwned')</script>")}
+	t4.Execute(w, data)
 	fmt.Println(t4.Name())
+}
+
+
+
+
+
+
+
+func main() {
+	http.HandleFunc("/test1", test1Handler);
+	http.HandleFunc("/test2", test2Handler);
+	http.HandleFunc("/test3", test3Handler);
+	http.HandleFunc("/test4", test4Handler);
+
+	http.ListenAndServe(":4000", nil);
 }
 
 //注入模板的函数
