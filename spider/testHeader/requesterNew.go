fb961952b693e4a967835e051cb76fa9016b62a2
silver.sun <itisagift@yeah.net>
2018-12-05 11:27:05 +0800

    test new header

--- spider/testHeader/requesterNew.go
+++ spider/testHeader/requesterNew.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "http://localhost:8080/test", nil)
+	req.Header.Set("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36`)
 	fmt.Println("Sending the go origin Request")
-	_, _ = http.Get("http://localhost:8080/test")
+	_, _ = client.Do(req)
 }
